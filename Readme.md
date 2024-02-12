335806f01f8cc92748ab883f05afea3528fc196d
SarahNelviandri <sarah.210170236@mhs.unimal.ac.id>
2024-02-12 18:18:33 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+1. rubah nama folder menjadi perpustakaan
+2. menggunakan php 7.4.10
---
+++ database/db_perpustakaan.sql
@@ -0,0 +1,340 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Feb 2024 pada 13.15
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul_buku` varchar(125) NOT NULL,
+  `kategori_buku` varchar(125) NOT NULL,
+  `penerbit_buku` varchar(125) NOT NULL,
+  `pengarang` varchar(125) NOT NULL,
+  `tahun_terbit` varchar(125) NOT NULL,
+  `isbn` int(50) NOT NULL,
+  `j_buku_baik` varchar(125) NOT NULL,
+  `j_buku_rusak` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `kategori_buku`, `penerbit_buku`, `pengarang`, `tahun_terbit`, `isbn`, `j_buku_baik`, `j_buku_rusak`) VALUES
+(4, ' Pelaku dan Saksi Sejarah: Angkatan 66  Sumatera Barat Bertutur tentang Tritura', 'Sejarah', 'Erka (CV Rumah Kayu)', 'Nasrul Djalal &  Makmur Hendrik', '2019', 2147483647, '10', '0'),
+(5, ' Better Life with Action: 10 Langkah Sukses Usia Muda', 'Majalah', ' Jawara Bisnis Grup', 'Widyoko, Yoyok', '2009', 2147483647, '10', '0'),
+(6, 'Siapa Bilang Keadilan Itu Mahal: Kegelisahan  H.M Zuchli Imran Putra, SH., M.H Di Gerbang  Kesuksesan', 'Fiksi', 'PT Jambogate', 'Hidayat, M. Syahnoer', '2011', 2147483647, '10', '0'),
+(7, ' Women In Minangkabau Nagari Goverment: Gender  Negotiating, and Contesting Power at The Local Level', 'Non FIksi', 'Erka (CV Rumah Kayu)', 'Jendrius  Editor: Tanjung Alizar', '2007', 2147483647, '10', '0'),
+(8, 'My Big Dream: Meraih Impian Terbesar Hidupku', 'Fiksi', 'Diniyyah Research  Centre', 'Fauziah, Fauzan', '2013', 2147483647, '10', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `identitas`
+--
+
+CREATE TABLE `identitas` (
+  `id_identitas` int(11) NOT NULL,
+  `nama_app` varchar(50) NOT NULL,
+  `alamat_app` text NOT NULL,
+  `email_app` varchar(125) NOT NULL,
+  `nomor_hp` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `identitas`
+--
+
+INSERT INTO `identitas` (`id_identitas`, `nama_app`, `alamat_app`, `email_app`, `nomor_hp`) VALUES
+(1, 'Perpustakaan SMAN 2 Padang', 'Jl. Musi No.2, Rimbo Kaluang, Kec. Padang Bar., Kota Padang, Sumatera Barat', 'perpussma2padang@gmail.com', '6281221545666');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `kode_kategori` varchar(50) NOT NULL,
+  `nama_kategori` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `kode_kategori`, `nama_kategori`) VALUES
+(1, 'KT-001', 'Novel '),
+(2, 'KT-002', 'Cergam'),
+(3, 'KT-003', 'Ensiklopedi'),
+(4, 'KT-004', 'Biografi'),
+(5, 'KT-005', 'Catatan Harian'),
+(6, 'KT-006', 'Karya Ilmiah'),
+(7, 'KT-007', 'Tafsir'),
+(8, 'KT-008', 'Panduan (how to)'),
+(9, 'KT-009', 'Majalah'),
+(10, 'KT-010', 'Antologi'),
+(11, 'KT-011', 'Sejarah'),
+(12, 'KT-012', 'Fiksi'),
+(13, 'KT-013', 'Non FIksi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pemberitahuan`
+--
+
+CREATE TABLE `pemberitahuan` (
+  `id_pemberitahuan` int(11) NOT NULL,
+  `isi_pemberitahuan` varchar(255) NOT NULL,
+  `level_user` varchar(125) NOT NULL,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pemberitahuan`
+--
+
+INSERT INTO `pemberitahuan` (`id_pemberitahuan`, `isi_pemberitahuan`, `level_user`, `status`) VALUES
+(1, '<i class=\'fa fa-exchange\'></i> #Reza  Saputra Telah meminjam Buku', 'Admin', 'Belum dibaca'),
+(2, '<i class=\'fa fa-repeat\'></i> #Reza  Saputra Telah mengembalikan Buku', 'Admin', 'Belum dibaca'),
+(3, '<i class=\'fa fa-exchange\'></i> #Budi Hartano Telah meminjam Buku', 'Admin', 'Belum dibaca'),
+(4, '<i class=\'fa fa-repeat\'></i> #Budi Hartano Telah mengembalikan Buku', 'Admin', 'Belum dibaca');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(11) NOT NULL,
+  `nama_anggota` varchar(125) NOT NULL,
+  `judul_buku` varchar(125) NOT NULL,
+  `tanggal_peminjaman` varchar(125) NOT NULL,
+  `tanggal_pengembalian` varchar(50) NOT NULL,
+  `kondisi_buku_saat_dipinjam` varchar(125) NOT NULL,
+  `kondisi_buku_saat_dikembalikan` varchar(125) NOT NULL,
+  `denda` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `nama_anggota`, `judul_buku`, `tanggal_peminjaman`, `tanggal_pengembalian`, `kondisi_buku_saat_dipinjam`, `kondisi_buku_saat_dikembalikan`, `denda`) VALUES
+(1, 'Reza  Saputra', 'Cantik Itu Luka', '08-08-2022', '08-08-2022', 'Baik', 'Baik', 'Tidak ada'),
+(2, 'Budi Hartano', ' Pelaku dan Saksi Sejarah: Angkatan 66  Sumatera Barat Bertutur tentang Tritura', '11-02-2024', '11-02-2024', 'Baik', 'Baik', 'Tidak ada');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(11) NOT NULL,
+  `kode_penerbit` varchar(125) NOT NULL,
+  `nama_penerbit` varchar(50) NOT NULL,
+  `verif_penerbit` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `kode_penerbit`, `nama_penerbit`, `verif_penerbit`) VALUES
+(1, 'P001', 'Gramedia Pustaka Utama', 'Terverifikasi'),
+(2, 'P002', 'Mizan Pustaka', 'Terverifikasi'),
+(3, 'P003', 'Bentang Pustaka', 'Terverifikasi'),
+(4, 'P004', 'Erlangga', 'Terverifikasi'),
+(5, 'P005', 'Republika', 'Terverifikasi'),
+(6, 'P006', 'Erka (CV Rumah Kayu)', 'Terverifikasi'),
+(7, 'P007', ' Jawara Bisnis Grup', 'Terverifikasi'),
+(8, 'P008', 'PT Jambogate', 'Terverifikasi'),
+(9, 'P009', 'Diniyyah Research  Centre', 'Terverifikasi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesan`
+--
+
+CREATE TABLE `pesan` (
+  `id_pesan` int(11) NOT NULL,
+  `penerima` varchar(50) NOT NULL,
+  `pengirim` varchar(50) NOT NULL,
+  `judul_pesan` varchar(50) NOT NULL,
+  `isi_pesan` text NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `tanggal_kirim` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pesan`
+--
+
+INSERT INTO `pesan` (`id_pesan`, `penerima`, `pengirim`, `judul_pesan`, `isi_pesan`, `status`, `tanggal_kirim`) VALUES
+(2, 'Administrator', 'Budi Hartano', 'Buku hilang', 'Buk buku saya hilang', 'Sudah dibaca', '11-02-2024');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `kode_user` varchar(25) NOT NULL,
+  `nis` char(20) NOT NULL,
+  `fullname` varchar(125) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `kelas` varchar(50) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `verif` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `join_date` varchar(125) NOT NULL,
+  `terakhir_login` varchar(125) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `kode_user`, `nis`, `fullname`, `username`, `password`, `kelas`, `alamat`, `verif`, `role`, `join_date`, `terakhir_login`) VALUES
+(1, '-', '-', 'Administrator', 'admin', 'admin', '-', '-', 'Iya', 'Admin', '04-05-2021', '11-02-2024 ( 16:01:23 )'),
+(3, 'AP001', '1234567891', 'Budi Hartano', 'budi', 'budi', 'X - IPA', 'Padang', 'Tidak', 'Anggota', '11-02-2024', '11-02-2024 ( 16:17:46 )');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `identitas`
+--
+ALTER TABLE `identitas`
+  ADD PRIMARY KEY (`id_identitas`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `pemberitahuan`
+--
+ALTER TABLE `pemberitahuan`
+  ADD PRIMARY KEY (`id_pemberitahuan`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`);
+
+--
+-- Indeks untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indeks untuk tabel `pesan`
+--
+ALTER TABLE `pesan`
+  ADD PRIMARY KEY (`id_pesan`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `identitas`
+--
+ALTER TABLE `identitas`
+  MODIFY `id_identitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `pemberitahuan`
+--
+ALTER TABLE `pemberitahuan`
+  MODIFY `id_pemberitahuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  MODIFY `id_penerbit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesan`
+--
+ALTER TABLE `pesan`
+  MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
